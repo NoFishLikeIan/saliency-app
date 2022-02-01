fbe09062bc7fbb292ef821a7cf8fbf316d6a709c
NoFishLikeIan <andreatitton96@gmail.com>
2022-02-01 08:59:08 +0100

    🦋 https support

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,13 @@
+import os
+from flask_sslify import SSLify
+
 from app import app, db
 
 if __name__ == "__main__":
     with app.app_context():
         db.init_db()
+
+    if 'DYNO' is os.environ:
+        app = SSLify(app)
         
     app.run()
